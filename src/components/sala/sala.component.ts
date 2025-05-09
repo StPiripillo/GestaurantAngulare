5911ed2a041e2d22395d913cc7cd3913c27ccad1
Silvestro21 <21silvestrorinaldi@gmail.com>
2025-05-09 10:40:26 +0200

    cambioNumero terminato

--- src/components/sala/sala.component.ts
+++ src/components/sala/sala.component.ts
@@ -94,6 +94,13 @@ export class SalaComponent implements OnInit {
     if (nuovoNumero) {
       const numero = parseInt(nuovoNumero);
       if (!isNaN(numero)) {
+        //questo controlla se il numero gia esiste
+        const numeroEsistente = this.tavoli.some(t => t.numeroTavolo === numero && t.id !== tavolo.id);
+        if (numeroEsistente) {
+          alert("Errore: il numero del tavolo è già esistente.");
+          return;
+        }
+
         tavolo.numeroTavolo = numero;
         this.TavoloRepo.updateNomeTavolo(tavolo.id, numero).subscribe(() => {
           alert("Numero tavolo modificato");
